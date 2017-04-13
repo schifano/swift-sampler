ec8b143e4b479caf1a9c566bdeedb208a22b9fa7
Rachel Schifano <rschifa1@gmail.com>
2017-04-12 21:23:12 -0500

    Add TapCounter

---
+++ SwiftSampler/SwiftSampler/Style.swift
@@ -0,0 +1,17 @@
+//
+//  Style.swift
+//  SwiftSampler
+//
+//  Created by Rachel Schifano on 4/12/17.
+//  Copyright © 2017 schifano. All rights reserved.
+//
+
+import UIKit
+
+struct Style {
+
+    struct TapCounterViewController {
+        static let fontButton = UIFont(name: "AvenirNext-Regular", size: 20)
+        static let fontLabel = UIFont(name: "AvenirNext-Regular", size: 125)
+    }
+}
--- SwiftSampler/SwiftSampler/TapCounterViewController.swift
+++ SwiftSampler/SwiftSampler/TapCounterViewController.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class TapCounterViewController: UIViewController {
     
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counterButton.titleLabel?.font = Style.TapCounterViewController.fontButton
+        counterLabel.font = Style.TapCounterViewController.fontLabel
+        resetButton.titleLabel?.font = Style.TapCounterViewController.fontButton
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        counterLabel.sizeToFit()
+    }
+    
+    @IBAction func resetCounter(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "\(counter)"
+        counterLabel.sizeToFit()
+    }
 }
