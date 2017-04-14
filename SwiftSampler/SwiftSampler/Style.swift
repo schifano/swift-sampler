cf8286a9fb24f20d26b7f2da3116245283a1a794
Rachel Schifano <rschifa1@gmail.com>
2017-04-13 23:41:44 -0500

    Change aesthetic of tap or hold

--- SwiftSampler/SwiftSampler/Style.swift
+++ SwiftSampler/SwiftSampler/Style.swift
@@ -14,4 +14,16 @@ struct Style {
         static let fontButton = UIFont(name: "AvenirNext-Regular", size: 20)
         static let fontLabel = UIFont(name: "AvenirNext-Regular", size: 125)
     }
+    
+    struct TapOrHoldCounterViewController {
+        static let fontButton = UIFont(name: "AvenirNext-Regular", size: 20)
+        static let fontLabel = UIFont(name: "AvenirNext-Regular", size: 125)
+    }
+    
+    static func color(for hex: Int) -> UIColor {
+        return UIColor(red: CGFloat(Float((hex & 0xff0000) >> 16) / 255.0),
+                       green: CGFloat(Float((hex & 0x00ff00) >> 8) / 255.0),
+                       blue: CGFloat(Float((hex & 0x0000ff) >> 0) / 255.0),
+                       alpha: CGFloat(1.0))
+    }
 }
--- SwiftSampler/SwiftSampler/TapOrHoldCounterViewController.swift
+++ SwiftSampler/SwiftSampler/TapOrHoldCounterViewController.swift
@@ -9,6 +9,46 @@
 import UIKit
 
 class TapOrHoldCounterViewController: UIViewController {
-
-
+    
+    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    var counter = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counterButton.titleLabel?.font = Style.TapCounterViewController.fontButton
+        counterButton.setTitleColor(UIColor.black, for: .normal)
+        counterButton.backgroundColor = Style.color(for: 0x4cd964)
+        counterButton.layer.cornerRadius = 5
+        counterButton.layer.masksToBounds = true
+        
+        counterLabel.font = Style.TapCounterViewController.fontLabel
+        resetButton.titleLabel?.font = Style.TapCounterViewController.fontButton
+        
+        // add long press gesture recognizer
+        let longPress = UILongPressGestureRecognizer(target: self, action:#selector(TapOrHoldCounterViewController.buttonLongPress))
+        longPress.minimumPressDuration = 0.1
+        counterButton.addGestureRecognizer(longPress)
+    }
+    
+    func buttonLongPress(sender: UIButton) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        counterLabel.sizeToFit()
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        counterLabel.sizeToFit()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "\(counter)"
+        counterLabel.sizeToFit()
+    }
 }
